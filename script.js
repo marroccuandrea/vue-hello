46a9573a53a87980ea38bb34f06d2379bd3eb390
marroccuandrea <marroccuandrea@yahoo.it>
2024-03-11 20:26:38 +0100

    [add] function cambio colore numeri pari

--- script.js
+++ script.js
@@ -6,8 +6,32 @@ createApp({
         return{
             messaggio: 'Ciao',
             // BONUS 
-            immagine: './gargantua.jpg'
+            immagine: './gargantua.jpg',
+            testo: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Corrupti, rem.',
+            contatore: 0,
+            cambiaGiallo: 'giallo',
+            isViola: true,
+            isEven: false
         }
-    }
+    },
+
+methods:{
+    incrementaContatore(){
+        this.contatore++;
+        if (this.contatore % 2 === 0){
+            this.isEven = true;
+           }else {
+            this.isEven = false;
+           }
+    },
+
+    cambiaColore(){
+        this.isViola = !this.isViola;
+    },
+
+    
+}
 
 }).mount('#app');
+
+
